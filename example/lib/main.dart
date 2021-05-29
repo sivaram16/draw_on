bfa2de38f7cff0361df4347b1d08969d94e198ad
Sivaram <sivaram@tartlabs.com>
2021-05-29 17:41:42 +0530

    80% Completed core logic and as well as example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,47 +1,20 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:draw_on/draw_on.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DrawOnExample());
 }
 
-class MyApp extends StatefulWidget {
+class DrawOnExample extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  _DrawOnExampleState createState() => _DrawOnExampleState();
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
+class _DrawOnExampleState extends State<DrawOnExample> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await DrawOn.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -49,12 +22,274 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Draw On'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: SingleChildScrollView(
+            child: Wrap(
+              runSpacing: 50.0,
+              spacing: 80.0,
+              children: [
+                _DrawAreaExample(),
+                _SelectAreaExample(),
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _DrawOnImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "assets/heart.png",
+      width: 400,
+      height: 400,
+      fit: BoxFit.fill,
+    );
+  }
+}
+
+class _SelectAreaExample extends StatefulWidget {
+  @override
+  __SelectAreaExampleState createState() => __SelectAreaExampleState();
+}
+
+class __SelectAreaExampleState extends State<_SelectAreaExample> {
+  double? selectedPositionX;
+  double? selectedPositionY;
+  double coordinateSizeX = 1;
+  double coordinateSizeY = 1;
+  List<Offset> polygonPoints = [];
+  final List<List<Offset>> coordinates = [
+    [
+      Offset(170.7, 116.0),
+      Offset(131.7, 154.0),
+      Offset(162.7, 170.0),
+      Offset(219.7, 106.0),
+      Offset(183.7, 110.0),
+      Offset(170.7, 116.0)
+    ],
+    [
+      Offset(194.7, 208.0),
+      Offset(175.7, 258.0),
+      Offset(230.7, 252.0),
+      Offset(235.7, 198.0),
+      Offset(206.7, 201.0),
+      Offset(194.7, 208.0)
+    ]
+  ];
+  bool isSelectable = true;
+  List<List<Offset>> correctAnswerCoordinates = [];
+  bool isInside = false;
+  String answer = "";
+  bool showPointer = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Select anywhere on the image',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24),
+        ),
+        SizedBox(height: 10),
+        SelectAreaWidget(
+          correctAnswerCoordinates: correctAnswerCoordinates,
+          widget: _DrawOnImageWidget(),
+          isSelectable: isSelectable,
+          getXaxis: (xAxis) {
+            setState(() {
+              selectedPositionX = xAxis;
+            });
+          },
+          showPointer: showPointer,
+          getYaxis: (yAxis) {
+            setState(() {
+              selectedPositionY = yAxis;
+            });
+          },
+          coordinateSizeX: coordinateSizeX,
+          coordinateSizeY: coordinateSizeY,
+          onTap: () {
+            setState(() {
+              showPointer = true;
+              coordinateSizeX = 1.0;
+              coordinateSizeY = 1.0;
+            });
+          },
+          pointsColor: Colors.red,
+          lineColor: Colors.green,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Container(
+          height: 50,
+          width: 200,
+          margin: EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  for (final List<Offset> offset in coordinates) {
+                    for (int i = 0; i < offset.length; i++) {
+                      polygonPoints.add(offset[i]);
+                    }
+                  }
+                  Polygon polygon = Polygon(polygonPoints);
+                  setState(() {
+                    isInside = polygon.isPointInside(
+                        Offset(selectedPositionX!, selectedPositionY!));
+                    answer = isInside
+                        ? 'Point is inside the polygon'
+                        : 'Point is outside the polygon';
+                    isSelectable = false;
+                    correctAnswerCoordinates = coordinates;
+                  });
+                },
+                child: Text('Submit'),
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      answer = '';
+                      isSelectable = true;
+                      correctAnswerCoordinates = [];
+                      showPointer = false;
+                    });
+                  },
+                  child: Text('Reset'),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          answer,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16),
+        ),
+      ],
+    );
+  }
+}
+
+class _DrawAreaExample extends StatefulWidget {
+  @override
+  __DrawAreaExampleState createState() => __DrawAreaExampleState();
+}
+
+class __DrawAreaExampleState extends State<_DrawAreaExample> {
+  List<List<Offset>> selectedPositions = [[]];
+  bool isSelectable = true;
+  bool? isInside;
+  double coordinateSizeX = 1;
+  double coordinateSizeY = 1;
+  double? selectedX;
+  double? selectedY;
+  bool showPointer = true;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Draw anywhere on the image',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24),
+        ),
+        SizedBox(height: 10),
+        DrawOnWidget(
+          widget: _DrawOnImageWidget(),
+          correctAnswerCoordinates: selectedPositions,
+          getXaxis: (xAxis) {
+            setState(() {
+              selectedX = xAxis;
+            });
+          },
+          getYaxis: (yAxis) {
+            setState(() {
+              selectedY = yAxis;
+            });
+          },
+          showPointer: showPointer,
+          coordinateSizeX: coordinateSizeX,
+          coordinateSizeY: coordinateSizeY,
+          onTap: () {
+            setState(() {
+              showPointer = true;
+              if (selectedPositions.last.length > 3 &&
+                  selectedPositions.last.first == selectedPositions.last.last) {
+                selectedPositions.add([]);
+              }
+              coordinateSizeX = 1.0;
+              coordinateSizeY = 1.0;
+              Offset? nearestValue = Polygon.getNearestPoint(
+                  selectedPositions.last, Offset(selectedX!, selectedY!));
+              selectedPositions.last.add(Offset(selectedX!, selectedY!));
+              if (nearestValue != null) {
+                selectedPositions.last.add(nearestValue);
+              }
+              selectedX = null;
+              selectedY = null;
+            });
+            print(selectedPositions);
+          },
+          pointsColor: Colors.green,
+          lineColor: Colors.green,
+        ),
+        SizedBox(height: 15),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          height: 50,
+          width: 200,
+          child: Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isSelectable = true;
+                    selectedPositions.clear();
+                    selectedPositions = [[]];
+                    showPointer = false;
+                  });
+                },
+                child: Text('Reset'),
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showPointer = false;
+                    if (selectedPositions.isNotEmpty) {
+                      if (selectedPositions.last.isNotEmpty)
+                        selectedPositions.last.removeLast();
+                      if (selectedPositions.last.isEmpty) {
+                        selectedPositions.removeLast();
+                      }
+                    }
+                  });
+                },
+                child: Text('Undo'),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
